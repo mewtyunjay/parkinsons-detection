82e1d8dcc2faa1e73ca625436bb838c3359416de
Mrityunjay Abhijeet Bhanja <mabhijeet11@gmail.com>
2020-10-06 04:51:52 +0530

    final

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import jsonify
 import requests
 
 app = Flask(__name__)
-model = pickle.load(open('bgcl_classification_model.pkl', 'rb'))
+model = pickle.load(open('xgb-model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
@@ -19,13 +19,13 @@ def predict():
         prediction_proba = model.predict_proba(final_features)
         
         output = prediction_proba[0][1]
-
-        if(output>0.7):
-            return render_template('index.html', prediction_text="The patient is in danger. The chances of Parkinson's Disease in the patient are {}".format(output))
+        output = (round(output,4))*100
+        if(output>70.00):
+            return render_template('index.html', prediction_text="The patient is in danger. The chances of Parkinson's Disease in the patient are {:.2f}%".format(output))
         else:
-            return render_template('index.html', prediction_text="The patient is safe. The chances of Parkinson's Disease in the patient are very low.")
+            return render_template('index.html', prediction_text="The patient is safe. The chances of Parkinson's Disease in the patient are {:.2f}%".format(output))
 
 
 if __name__=="__main__":
     app.run(debug=True)
-
+    
